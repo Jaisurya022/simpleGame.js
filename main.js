c293bedbe1e18ccad3419e902d683fabad8ba520
Jaisurya022 <143484533+Jaisurya022@users.noreply.github.com>
2023-12-08 23:50:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,17 @@
-
 let playGame = confirm("Shall we play rock, paper, or scissors ?");
-
 if (playGame) {
- 
-    let playerChoice = prompt("please enter rock, paper, or scissors.");
-
-    if(playerChoice) {
-         
-        let playerOne = playerChoice.trim().toLowerCase ();
-        
-        if (
-            playerOne === "rock" || 
-            playerOne === "paper" ||
-            playerOne === "scissors" 
-         ) 
-         
-         {
+     let playerChoice = prompt("please enter rock, paper, or scissors.");
+if(playerChoice) {
+     let playerOne = playerChoice.trim().toLowerCase ();
+if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors")   {
            let computerChoice = Math.floor(Math.random()* 3 +1 );
-           
            let computer = computerChoice === 1 ?
-
             "rock"  : computerChoice === 2 ? 
             "paper" : 
             "scissors" ;
-
             let result =
              playerOne === computer 
              ? "Tie game!"
-
              : playerOne === "rock" && computer === "paper"
              ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
              :playerOne === "paper" && computer === "scissors"
@@ -38,7 +22,6 @@ if (playGame) {
              alert (result)
              let playAgain = confirm("play Again ?")  
              playAgain ? location.reload() : alert("Ok, thanks for playing");
-
          } else {
             alert("You did't enter rock, paper, or scissor");
          } 
