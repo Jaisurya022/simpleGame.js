d7b73ae313947552f7a4c56286e4556efe5da2b0
Jaisurya022 <143484533+Jaisurya022@users.noreply.github.com>
2023-12-08 23:42:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+
+let playGame = confirm("Shall we play rock, paper, or scissors ?");
+
+if (playGame) {
+ 
+    let playerChoice = prompt("please enter rock, paper, or scissors.");
+
+    if(playerChoice) {
+         
+        let playerOne = playerChoice.trim().toLowerCase ();
+        
+        if (
+            playerOne === "rock" || 
+            playerOne === "paper" ||
+            playerOne === "scissors" 
+         ) 
+         
+         {
+           let computerChoice = Math.floor(Math.random()* 3 +1 );
+           
+           let computer = computerChoice === 1 ?
+
+            "rock"  : computerChoice === 2 ? 
+            "paper" : 
+            "scissors" ;
+
+            let result =
+             playerOne === computer 
+             ? "Tie game!"
+
+             : playerOne === "rock" && computer === "paper"
+             ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+             :playerOne === "paper" && computer === "scissors"
+             ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+             :playerOne === "scissors" && computer === "rock"
+             ? `playerOne ${playerOne}\nComputer: ${computer}\nComputer wins!`
+             : `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+             alert (result)
+             let playAgain = confirm("play Again ?")  
+             playAgain ? location.reload() : alert("Ok, thanks for playing");
+
+         } else {
+            alert("You did't enter rock, paper, or scissor");
+         } 
+         
+    } else{
+            alert("I guess you changed your mind . Maybe next time .");
+           
+    }
+
+} else{
+        alert("Ok, Maybe next time");
+}
+
